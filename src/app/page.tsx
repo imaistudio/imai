60d342ae41d089a79f057419aea63931ebca1ae3
syed <syed@kemuri.in>
2025-05-07 13:50:07 +0530

    updated context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { MorphingText } from "@/components/magicui/morphing-text";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen min-w-full p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-center text-3xl font-bold">Hello World</h1>
+    <div className="flex items-center justify-center min-h-screen min-w-full p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white">
+      <MorphingText texts={["IMAI", "Genrate", "Newness"]} />
     </div>
   );
 }
