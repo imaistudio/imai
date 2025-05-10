f545a64c7ca4f7e5e508d99c17c3070024f8c798
syed <syed@kemuri.in>
2025-05-10 20:30:52 +0530

    removed unwanted files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
   description: "Generate Newness",
   other: {
     "apple-mobile-web-app-title": "IMAI",
+    "theme-color": "#ffffff",  // Set the theme color for mobile browsers
   },
 };
 
@@ -28,10 +29,11 @@ export default function RootLayout({
       <head>
         {/* Add these manually since Next.js doesn't allow all in metadata */}
         <link rel="icon" type="image/png" href="/icon1.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/faviconO.ico" />
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" /> {/* Add a theme color */}
       </head>
       <body className={`${spaceGrotesk.variable} antialiased`}>
         {children}
