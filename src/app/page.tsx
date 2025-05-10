8d253c9049919f8004de06301cb70e35b29469e7
syed <syed@kemuri.in>
2025-05-10 20:06:04 +0530

    added google font Space Grotesk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { MorphingText } from "@/components/magicui/morphing-text";
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen min-w-full p-8 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white">
-      <MorphingText texts={["IMAI", "Genrate", "Newness"]} />
+      <h1>Hello World</h1>
     </div>
   );
 }
