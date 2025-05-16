196b046a2995eac4bae1785f32ce8d8c1bbef05a
muqeethkemuri <syed@kemuri.in>
2025-05-17 04:21:00 +0530

    added profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,53 @@
+"use client"
+
+
+
+import { Button, Card, CardBody, CardFooter, CardHeader, Input } from "@heroui/react";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import {DatePicker} from "@heroui/react";
+import MinimalRowSteps from "../components/minimal-row-steps";
+import React from "react";
+const STEPS_COUNT = 2;
+
 export default function Profile() {
+  const [currentStep, setCurrentStep] = React.useState(1);
   return (
     <>
     <Header></Header>
-    <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Profile Page</h1>
-    </div>
+    <main className="flex min-h-screen bg-black flex-col items-center justify-center p-4 md:p-24">
+      <div className="w-full max-w-md">
+        <Card className="w-full bg-black">
+        <CardHeader className="flex flex-col items-center">
+    
+           <h1 className="text-3xl font-bold text-center my-4">Tell Us About You</h1>
+        </CardHeader>
+        <CardBody>
+          <Input
+              isRequired
+              label="Full Name"
+              name="fullName"
+              placeholder="Enter your full name"
+              type="text"
+              variant="underlined"
+            />
+            <DatePicker  variant="underlined" className="mt-4" label="Birth Date" />
+        </CardBody>
+        <CardFooter className="flex justify-center flex-col text-center text-sm text-muted-foreground">
+          <MinimalRowSteps
+            currentStep={currentStep}
+            label={`Step ${currentStep} of ${STEPS_COUNT}`}
+            stepsCount={STEPS_COUNT}
+            onStepChange={setCurrentStep}
+          />
+          <Button className="w-full my-2" color="primary" type="submit">
+              Continue
+            </Button>
+          <p className="mb-2">IMAI Creating Newness.</p>
+        </CardFooter>
+    </Card>
+      </div>
+    </main>
     <Footer></Footer>
     </>
   );
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -126,39 +126,7 @@ export default function Signup() {
         </div>
       </div>
 
-      {/* Right side */}
-      {/* <div
-        className="relative hidden w-1/2 flex-col-reverse rounded-medium p-10 shadow-small lg:flex"
-        style={{
-          backgroundImage:
-            "url(https://nextuipro.nyc3.cdn.digitaloceanspaces.com/components-images/white-building.jpg)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="flex flex-col items-end gap-4">
-          <User
-            avatarProps={{
-              src: "https://i.pravatar.cc/150?u=a04258a2462d826712d",
-            }}
-            classNames={{
-              base: "flex flex-row-reverse",
-              name: "w-full text-right text-black",
-              description: "text-black/80",
-            }}
-            description="Founder & CEO at ACME"
-            name="Bruno Reichert"
-          />
-          <p className="w-full text-right text-2xl text-black/60">
-            <span className="font-medium">“</span>
-            <span className="font-normal italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget augue nec massa
-              volutpat aliquet.
-            </span>
-            <span className="font-medium">”</span>
-          </p>
-        </div>
-      </div> */}
+    
 
       <div className="relative hidden w-1/2 flex-col-reverse rounded-medium p-10 shadow-small lg:flex overflow-hidden">
         {/* Background video */}
