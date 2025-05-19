8daacec2d10b6b59d60e837004e4235237356eb0
muqeethkemuri <syed@kemuri.in>
2025-05-19 17:54:08 +0530

    added firebase config

---
+++ lib/firebase.ts
@@ -0,0 +1,30 @@
+// lib/firebase.ts
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
+
+// Config from your Firebase Console
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
+};
+
+// Initialize app
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const realtimeDB = getDatabase(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { auth, firestore, realtimeDB, storage, analytics };
