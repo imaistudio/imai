610bcbe5feedc39441dc1070c7ba86f46f2e2889
muqeethkemuri <syed@kemuri.in>
2025-05-24 02:58:05 +0530

    added dummy content in the pages

---
+++ app/otp/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+export default function Otp() {
+  return (
+    <>
+    <Header></Header>
+    <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Otp Page</h1>
+    </div>
+    <Footer></Footer>
+    </>
+  );
+}
---
+++ app/phone/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+export default function Phone() {
+  return (
+    <>
+    <Header></Header>
+    <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Phone Page</h1>
+    </div>
+    <Footer></Footer>
+    </>
+  );
+}
