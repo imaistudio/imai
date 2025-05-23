9c561dab9791e38feabed9e99d21b7e8332bce63
muqeethkemuri <syed@kemuri.in>
2025-05-24 02:59:23 +0530

    removed header and footer from the otp | Phone

--- app/phone/page.tsx
+++ app/phone/page.tsx
@@ -1,13 +1,9 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
 export default function Phone() {
   return (
     <>
-    <Header></Header>
     <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <h1>Phone Page</h1>
     </div>
-    <Footer></Footer>
     </>
   );
 }
