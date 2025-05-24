3fb947429c08388eaa87354f08afd16efad4f386
muqeethkemuri <syed@kemuri.in>
2025-05-24 06:14:58 +0530

    added contry selector

--- app/phone/page.tsx
+++ app/phone/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+import { useState } from "react";
+import { CountryDropdown } from "../components/country-dropdown";
+import Header from "../components/header";
+
 export default function Phone() {
+  const [selectedAlpha3, setSelectedAlpha3] = useState("USA");
+
   return (
     <>
+    <Header></Header>
     <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Phone Page</h1>
+      <div className="w-1/4">
+        <CountryDropdown
+          placeholder="Select country"
+          defaultValue={selectedAlpha3}
+          onChange={(country) => {
+            console.log("Selected country:", country);
+            setSelectedAlpha3(country.alpha3);
+          }}
+        />
+      </div>
     </div>
     </>
   );
