87c489046d61390e1432acbe984dccc2c7b515c4
muqeethkemuri <syed@kemuri.in>
2025-05-25 20:58:10 +0530

    removed unnessary imports

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -3,15 +3,10 @@
 import * as React from "react"
 import {
   AudioWaveform,
-  BookOpen,
   Bot,
   Command,
-  Frame,
   GalleryVerticalEnd,
-  Map,
-  PieChart,
   Settings2,
-  SquareTerminal,
 } from "lucide-react"
 
 import {
