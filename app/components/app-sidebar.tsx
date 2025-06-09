1eee195de2bd79177c61769294b06d0cd1c5f278
syed abdul muqeeth <samx123786@gmail.com>
2025-06-09 22:48:39 +0530

    added backup nav secondary

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,40 +1,57 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
-  AudioWaveform,
-  Bot,
-  Command,
-  GalleryVerticalEnd,
-  Settings2,
-} from "lucide-react"
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarRail,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import { IMAIIcon } from "./imai";
+import { SearchForm } from "./search-form";
+import { NavSecondary } from "./nav-secondary";
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarRail,
-} from "@/components/ui/sidebar"
-import { NavUser } from "./nav-user"
-import { IMAIIcon } from "./imai"
-import { SearchForm } from "./search-form"
+	BookOpen,
+	Bot,
+	Command,
+	Frame,
+	LifeBuoy,
+	Map,
+	PieChart,
+	Send,
+	Settings2,
+	SquareTerminal,
+} from "lucide-react";
 
-
+const data = {
+	navSecondary: [
+		{
+			title: "Support",
+			url: "#",
+			icon: LifeBuoy,
+		},
+		{
+			title: "Feedback",
+			url: "#",
+			icon: Send,
+		},
+	],
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar  {...props}>
-      <SidebarHeader>
-        <IMAIIcon size={30} />  
-        <SearchForm></SearchForm>    
-      </SidebarHeader>
-
-      <SidebarContent>
-      </SidebarContent>
-      <SidebarFooter>
-        <NavUser />
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
-  )
+	return (
+		<Sidebar {...props}>
+			<SidebarHeader>
+				<IMAIIcon size={30} />
+				<SearchForm></SearchForm>
+			</SidebarHeader>
+			<SidebarContent></SidebarContent>
+			<SidebarFooter>
+				<NavUser />
+			</SidebarFooter>
+			<SidebarRail />
+		</Sidebar>
+	);
 }
