3f3aeb4b72d4c04fa6f38f6e358758966432acf8
syed abdul muqeeth <samx123786@gmail.com>
2025-06-09 23:03:37 +0530

    added autocomplete off

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -9,43 +9,14 @@ import {
 	SidebarRail,
 } from "@/components/ui/sidebar";
 import { NavUser } from "./nav-user";
-import { IMAIIcon } from "./imai";
 import { SearchForm } from "./search-form";
-import { NavSecondary } from "./nav-secondary";
-import {
-	BookOpen,
-	Bot,
-	Command,
-	Frame,
-	LifeBuoy,
-	Map,
-	PieChart,
-	Send,
-	Settings2,
-	SquareTerminal,
-} from "lucide-react";
 
-const data = {
-	navSecondary: [
-		{
-			title: "Support",
-			url: "#",
-			icon: LifeBuoy,
-		},
-		{
-			title: "Feedback",
-			url: "#",
-			icon: Send,
-		},
-	],
-};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 	return (
 		<Sidebar {...props}>
 			<SidebarHeader>
-				<IMAIIcon size={30} />
-				<SearchForm></SearchForm>
+			<SearchForm></SearchForm>
 			</SidebarHeader>
 			<SidebarContent></SidebarContent>
 			<SidebarFooter>
--- app/components/search-form.tsx
+++ app/components/search-form.tsx
@@ -9,7 +9,7 @@ import {
 
 export function SearchForm({ ...props }: React.ComponentProps<"form">) {
   return (
-    <form {...props}>
+    <form autoComplete="off" {...props}>
       <SidebarGroup className="py-2 border-red-500">
         <SidebarGroupContent className="relative">
           <Label htmlFor="search" className="sr-only">
