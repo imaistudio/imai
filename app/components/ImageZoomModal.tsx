384343e3c5cb273a64d444b7e710f59f53d98520
syed abdul muqeeth <samx123786@gmail.com>
2025-07-17 01:54:29 +0530

    fixing image zoom modal z-index issues

--- app/components/ImageZoomModal.tsx
+++ app/components/ImageZoomModal.tsx
@@ -160,7 +160,7 @@ export const ImageZoomModal = ({
 
       {isOpen && (
         <div
-          className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black/90"
+          className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black/90"
           onClick={handleClose}
         >
           {/* Header with close button and action icons */}
